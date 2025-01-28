ac0945006cfc7e804a7730005e5ecfedcf4e3372
Filip Sluzewski <12719849+mfs4rd@users.noreply.github.com>
2025-01-28 13:00:29 -0800

    added camera offset correction function

--- EMT_data_analysis/analysis_scripts/Image_alignment.py
+++ EMT_data_analysis/analysis_scripts/Image_alignment.py
@@ -1,6 +1,7 @@
 ######---------importing libraries--------#######
 import numpy as np
 import pickle as pkl
+from typing import Tuple
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -39,3 +40,34 @@ def get_alignment_matrix(barcode, alignment_folder='/allen/aics/assay-dev/users/
     '''
     matrix = pkl.load(open(f'{alignment_folder}/{barcode}_alignmentmatrix.pkl', 'rb'))
     return SimilarityTransform(matrix=matrix)
+
+def camera_correction(img: np.ndarray, offset: Tuple[int,int] = (8, 14)) -> np.ndarray:
+    '''
+    This function corrects a camera fov missalignment that occured in wells imaging N-Cad and E-Cad.
+    The the missalignment is corrected by cropping the top left and bottom right corners of channels
+    1/4 and 2/3/5, respectively so that only the overlapping fov remains.
+    
+    Parameters
+    ----------
+    img: np.ndarray
+        The 4D/3D (C,Z,Y,X)/(Z,Y,X) image that needs camera offset to be corrected. If the image is
+            4D it will apply to each channel as appropriately. For 3D (assumed brightfield mask) it
+            will crop only the top-left corner
+    offset: Tuple(int,int)
+        Offset of the camera fov's. Default: (8,14)
+    ''' 
+    assert img.ndim == 4 or img.ndim == 3, f"Invalid image shape {img.ndim}D. Must be 3D or 4D."
+
+    def crop(img_ch: np.ndarray, top_left=True):
+        if top_left:
+            return img_ch[:, offset[0]+1:, offset[1]+1:]
+        else:
+            return img_ch[:, :-offset[0], :-offset[1]]
+    
+    if img.ndim == 3:
+        img = np.expand_dims(img, 0)
+
+    # Indeces of channels in camera 1
+    cam1 = [0,3]
+    return np.stack([crop(img[ch], ch in cam1) for ch in range(img.shape[0])], axis=0).squeeze()
+
