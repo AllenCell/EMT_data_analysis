c79196bcf8b0b2f567295c09ad970d5e82dda15d
Filip Sluzewski <12719849+mfs4rd@users.noreply.github.com>
2025-10-30 09:33:28 -0700

    updating mesh figure generation

--- EMT_data_analysis/tools/io.py
+++ EMT_data_analysis/tools/io.py
@@ -7,7 +7,7 @@ def convert_to_windows_path(linux_path: Path):
 
 def load_imaging_and_segmentation_dataset():
     df = pd.read_csv("https://allencell.s3.amazonaws.com/aics/emt_timelapse_dataset/manifests/imaging_and_segmentation_data.csv")
-    n_movies = df['Movie ID'].nunique()
+    n_movies = df['Data ID'].nunique()
     print(f'Total number of movies in the dataset: {n_movies}')
     return df
 
